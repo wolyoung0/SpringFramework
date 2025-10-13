31f40454f6ac1b72cc6465c24f14d74c01f7f6e3
Changwook01 <tjckddnr3026@naver.com>
2025-10-13 16:02:59 +0900

    7주차

--- C_Ban/SBB/src/main/java/com/mysite/sbb/question/service/QuestionService.java
+++ C_Ban/SBB/src/main/java/com/mysite/sbb/question/service/QuestionService.java
@@ -8,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,7 +26,9 @@ public class QuestionService {
 
     public Page<Question> getList(int page) {
 
-        Pageable pageable = PageRequest.of(page, 10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("created"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         Page<Question> paging = questionRepository.findAll(pageable);
 
         return paging;
